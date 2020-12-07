3740efeb97d4b939e17701cad79c5510fd0ce0eb
amakaohiku <amakanza1@gmail.com>
2020-12-07 11:17:47 -0700

    done!

---
+++ index.js
@@ -0,0 +1,31 @@
+function sum(sum1, sum2){
+    return sum1 + sum2
+}
+var result = sum(10, 5)
+console.log(result)
+
+
+
+function get_max(num1, num2, num3)
+{
+    var max = Math.max(num1, num2, num3);
+    return max;
+}
+var result = get_max(30, 20, 10)
+console.log(result)
+
+
+
+
+
+
+function evenOrOdd(num) {
+    if (num % 2 == 0) 
+    {
+      return "even";
+    } else {
+      return "odd";
+    }
+};
+  var result = evenOrOdd(10)
+  console.log(result)
